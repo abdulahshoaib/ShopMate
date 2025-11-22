428a2c122d9ef683d4f88dcfb45267c1ffe22a52
bissamiftikhar <bissamiftikhar@gmail.com>
2025-11-22 21:11:24 +0500

    added DTOs

---
+++ ShopMate/DTOs/BillDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMate.DTOs
+{
+    internal class BillDTO
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public List<BillItemDTO> Items { get; set; } = new List<BillItemDTO>();
+        public decimal Total => Items.Sum(i => i.Total);
+    }
+}
