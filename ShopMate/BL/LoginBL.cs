54dbfc3a9b2241d34cff734851f466aac4e48610
awais <awaisakhtar205@gmail.com>
2025-11-29 02:41:14 +0500

    avoided exception in loginBL

--- ShopMate/BL/LoginBL.cs
+++ ShopMate/BL/LoginBL.cs
@@ -1,43 +1,44 @@
 ﻿using ShopMate.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ShopMate.DL;
-using ShopMate.DTOs;
 using ShopMate.GUI;
 
 namespace ShopMate.BL
 {
     internal class LoginBL
     {
-        private LoginDTO logindto;
         private LoginDL loginDL;
         private UserDTO userdto;
 
         public LoginBL()
         {
             loginDL = new LoginDL();
+
         }
 
         // Example function
         public bool loginuser(LoginDTO logindto)
         {
-            this.logindto = logindto;
-            userdto= loginDL.ValidateLogin(this.logindto);
-            if (userdto.Role == "Admin")
+            userdto = loginDL.ValidateLogin(logindto);
+            if (userdto != null)
             {
-                App.MainFrame?.Navigate(typeof(AdminDashboardPage));
-                return true;
+                if (userdto.Role == "Admin")
+                {
+                    App.MainFrame?.Navigate(typeof(AdminDashboardPage));
+                    return true;
+                }
+                else if (userdto.Role == "Salesperson")
+                {
+                    App.MainFrame?.Navigate(typeof(SalesPersonDashboardPage));
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
             }
-            else if (userdto.Role == "Salesperson")
+            else 
             {
-                App.MainFrame?.Navigate(typeof(SalesPersonDashboardPage));
-                return true;
-            }
-            else { 
-            return false;
+                return false;
             }
         }
     }
