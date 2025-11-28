876870e497d0214b51705795332c75e7a8d6fd1c
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-29 00:05:28 +0500

    feat: added the product management functions

---
+++ ShopMate/BL/ProductManagementBL.cs
@@ -0,0 +1,35 @@
+﻿using ShopMate.DL;
+using ShopMate.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMate.BL
+{
+    public class ProductManagementBL
+    {
+        private readonly ProductManagementDL pmDL;
+
+        public ProductManagementBL()
+        {
+            pmDL = new ProductManagementDL();
+        }
+
+        public bool AddProduct(ProductDTO pDTO)
+        {
+            return pmDL.AddProduct(pDTO);
+        }
+
+        public bool RemoveProduct(ProductDTO pDTO)
+        {
+            return pmDL.RemoveProduct(pDTO.ID);
+        }
+
+        public bool UpdateProduct(ProductDTO pDTO)
+        {
+            return pmDL.UpdateProduct(pDTO);
+        }
+    }
+}
