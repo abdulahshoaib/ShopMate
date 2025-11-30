329464756a2fcf1eaf1a80347ba01b041cc60559
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-11-30 16:55:58 +0500

    feat: added hash checking to passwords

--- ShopMate/DL/LoginDL.cs
+++ ShopMate/DL/LoginDL.cs
@@ -1,7 +1,8 @@
 using ShopMate.DTOs;
+using Supabase.Postgrest;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Supabase.Postgrest;
 
 namespace ShopMate.DL
 {
@@ -11,15 +12,23 @@ namespace ShopMate.DL
         {
             var client = SupabaseInitializer.client;
 
+            string hashed = HashPassword(loginDTO.Password);
+
             // Fetch user by username and password
             var response = await client.From<UserDTO>()
                                        .Where(u => u.Username == loginDTO.Username &&
-                                                   u.PasswordHash == loginDTO.Password)
+                                                   u.PasswordHash == hashed)
                                        .Get();
 
             var user = response.Models.FirstOrDefault();
 
             return user;
+        }
+        private string HashPassword(string password)
+        {
+            using var sha = System.Security.Cryptography.SHA256.Create();
+            var bytes = sha.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
         }
     }
 }
